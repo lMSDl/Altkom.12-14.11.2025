81292f2baa764a851fb0c60bbedb2e16d9d6a183
Paweł <pawel@msd.info.pl>
2025-11-12 11:33:11 +0100

    REST

--- WebApi/Controllers/ShoppingListsController.cs
+++ WebApi/Controllers/ShoppingListsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Models;
 
 namespace WebApi.Controllers
 {
@@ -12,32 +13,71 @@ namespace WebApi.Controllers
         }
 
 
-        public IEnumerable<Models.ShoppingList> Get()
+        //Aby móc zastosować metody pomocnicze REST używamy jako typu zwracanego IActionResult, ActionResult lub ActionResult<T>
+        public ActionResult<IEnumerable<ShoppingList>> Get()
         {
-            return _shoppingLists;
+            //metoda pomocnicze Ok() - zwraca kod statusu 200 wraz z danymi
+            return Ok(_shoppingLists);
         }
 
         [HttpGet("{id:int}")]
-        public Models.ShoppingList? GetById(int id)
+        public ActionResult<ShoppingList> GetById(int id)
         {
-            return _shoppingLists.FirstOrDefault(x => x.Id == id);
+            var entity= _shoppingLists.FirstOrDefault(x => x.Id == id);
+
+            if (entity is null)
+                //metoda pomocnicza NotFound() - zwraca kod statusu 404
+                return NotFound();
+
+            return Ok(entity);
         }
 
         [HttpPost]
-        public void Post(Models.ShoppingList shoppingList)
+        public ActionResult<int> Post(Models.ShoppingList shoppingList)
         {
             shoppingList.Id = _shoppingLists.Select(x => x.Id).DefaultIfEmpty().Max() + 1;
             _shoppingLists.Add(shoppingList);
+
+            //zamiast ręcznie konfigurować odpowiedź HTTP, używamy metody pomocniczej CreatedAtAction()
+            //HttpContext.Response.StatusCode = 201; //Created
+
+            /*return CreatedAtAction(
+                nameof(GetById),
+                new { id = shoppingList.Id },
+                shoppingList);*/ //tutaj zwracamy cały obiekt
+
+            //metoda CreatedAtAction pozwala na wskazanie funkcji, która może zostać użyta do pobrania nowo utworzonego zasobu
+            return CreatedAtAction(
+                nameof(GetById),
+                new { id = shoppingList.Id }, //obiekt anonimowy z parametrami trasy - nazwa "id" to nazwa parametru z metody GetById
+                shoppingList.Id); //tutaj zwracamy tylko Id nowo utworzonego zasobu
         }
 
         [HttpPut("{id:int}")]
-        public void Put(int id, Models.ShoppingList shoppingList)
+        public ActionResult Put(int id, Models.ShoppingList shoppingList)
         {
             var index = _shoppingLists.ToList().FindIndex(x => x.Id == id);
-            if (index != -1)
+            if (index == -1)
             {
-                _shoppingLists[index] = shoppingList;
+                return NotFound();
             }
+
+            _shoppingLists[index] = shoppingList;
+
+            return NoContent(); //204 No Content
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var entity = _shoppingLists.FirstOrDefault(x => x.Id == id);
+            if (entity is null)
+                return NotFound();
+
+
+            _shoppingLists.Remove(entity);
+
+            return NoContent(); //204 No Content
         }
     }
 }
