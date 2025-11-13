c0736cf9489202c118695e99c04011760e7d1d14
Paweł <pawel@msd.info.pl>
2025-11-13 15:28:39 +0100

    OpenAPI client

---
+++ ConsoleAppOpenAPI/Program.cs
@@ -0,0 +1,10 @@
+﻿using MyWebApi;
+
+using var httpClient = new HttpClient();
+contractClient client = new contractClient(httpClient);
+
+var shoppingLists = await client.ShoppingListsAllAsync();
+
+var people = await client.PeopleAllAsync("");
+
+Console.ReadLine();
---
+++ ConsoleAppOpenAPI/contractClient.jsonSettings.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace MyWebApi
+{
+    public partial class contractClient
+    {
+        partial void UpdateJsonSerializerSettings(Newtonsoft.Json.JsonSerializerSettings settings)
+        {
+            settings.PreserveReferencesHandling = PreserveReferencesHandling.Objects;
+        }
+    }
+}
